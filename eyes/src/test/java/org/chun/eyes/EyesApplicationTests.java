e54ecf5f46c7b55d0abd67ac34925d2ae6838ff6
GD <gordon.fu@neutec.com.tw>
2023-08-07 14:13:57 +0800

    eyes

---
+++ eyes/src/test/java/org/chun/eyes/EyesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chun.eyes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
